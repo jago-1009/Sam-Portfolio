7766527290f800754ef43f30f86b67346316a1b5
jago-1009 <garwoodj22@gmail.com>
2025-05-28 10:40:35 -0400

    feat: add project modals and contact section; enhance project display

--- app.js
+++ app.js
@@ -18,17 +18,18 @@ window.onload = function () {
                 <div class="project-bio">
                 <h3>${project.name}</h3>
                 <p>${project.teaser}</p>
-               <button onclick="window.project_${index}_dialog.showModal()" class="learn-more">Learn more about ${project.name}</button></div>
+               
+               <button onclick="window.game_project_${index}_dialog.showModal()" class="learn-more">Learn more about ${project.name}</button></div>
                  </div>
                  
-                 <dialog id="project_${index}_dialog" class="dialog">
-                <button onclick="window.project_${index}_dialog.close()" class="close-button" id="close-${index}">X</button>
+                 <dialog id="game_project_${index}_dialog" class="dialog">
+                <button onclick="window.game_project_${index}_dialog.close()" class="close-button" id="close-${index}">X</button>
                  <div class="dialog-content">
                  ${url}
                     <h2>${project.name}</h2>
                     
                     <p>${project.description}</p>
-                    <a href="${project.button_url}" target="_blank" class="button">${project.button_text}</a>
+                    <a href="${project.button_link}" target="_blank" class="learn-more">${project.button_text}</a>
                  
                 
                     </div>
@@ -41,7 +42,67 @@ window.onload = function () {
           
            <div class="project-bio">
             <h4>${project.name}</h4>
-            <p>${project.description}</p>
+            <p>${project.teaser}</p>
+<button onclick="window.game_project_${index}_dialog.showModal()" class="learn-more">Learn more about ${project.name}</button></div>
+                 </div>
+                 
+                 <dialog id="game_project_${index}_dialog" class="dialog">
+                <button onclick="window.game_project_${index}_dialog.close()" class="close-button" id="close-${index}">X</button>
+                 <div class="dialog-content">
+                 ${url}
+                    <h2>${project.name}</h2>
+                    
+                    <p>${project.description}</p>
+                    <a href="${project.button_link}" target="_blank" class="learn-more">${project.button_text}</a>
+                 
+                 
+                
+                    </div>
+                </dialog>
+                    </div>`;
+        }
+      });
+      data.writing_projects.map((project, index) => {
+        let url = "";
+        if (project.photo_type == "image") {
+          url = `<img class="project-image" src="${project.photo_url}" alt="${project.name}"></img>`;
+        } else if (project.photo_type == "video") {
+          url = `<iframe class="youtube-video" src="${project.video_url}" title="${project.name}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+        }
+        if (index % 2 === 0) {
+          document.getElementById(
+            "writing-projects-container"
+          ).innerHTML += `<div class="project left">
+              
+               
+                <div class="project-bio">
+                <h3>${project.name}</h3>
+                <p>${project.teaser}</p>
+               
+               <button onclick="window.project_${index}_dialog.showModal()" class="learn-more">Learn more about ${project.name}</button></div>
+                 </div>
+                 
+                 <dialog id="project_${index}_dialog" class="dialog">
+                <button onclick="window.project_${index}_dialog.close()" class="close-button" id="close-${index}">X</button>
+                 <div class="dialog-content">
+                 ${url}
+                    <h2>${project.name}</h2>
+                    
+                    <p>${project.description}</p>
+                    <a href="${project.button_link}" target="_blank" class="learn-more">${project.button_text}</a>
+                 
+                
+                    </div>
+                </dialog>
+                    </div>`;
+        } else {
+          document.getElementById(
+            "writing-projects-container"
+          ).innerHTML += `<div class="project right">
+          
+           <div class="project-bio">
+            <h4>${project.name}</h4>
+            <p>${project.teaser}</p>
 <button onclick="window.project_${index}_dialog.showModal()" class="learn-more">Learn more about ${project.name}</button></div>
                  </div>
                  
@@ -52,7 +113,7 @@ window.onload = function () {
                     <h2>${project.name}</h2>
                     
                     <p>${project.description}</p>
-                    <a href="${project.button_url}" target="_blank" class="button">${project.button_text}</a>
+                    <a href="${project.button_link}" target="_blank" class="learn-more">${project.button_text}</a>
                  
                  
                 
@@ -62,5 +123,6 @@ window.onload = function () {
         }
       });
     })
+
     .catch((error) => console.error("Error fetching JSON:", error));
 };
